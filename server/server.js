dbff3f8b24169760316f7f3de4cc1753897c7eb3
nuno manuel goncalves da silva pereira <schwarz14x@gmail.com>
2024-09-10 15:43:11 +0100

    up

--- server/server.js
+++ server/server.js
@@ -26,15 +26,25 @@ app.post("/messages", async function (request, response) {
   const message = request.body.message;
   const newMessage = await db.query(
     "INSERT INTO messages (name, message, likes, dislikes) VALUES ($1, $2, $3, $4)",
-    [name, message, 0]
+    [name, message, 0, 1]
   );
   response.json(newMessage.rows[0]);
 });
 app.post("/messages/:id/like", async function (request, response) {
-  const messageId = resquest.params.messageId;
+  const messageId = request.params.id;
   {
     const updatedMessage = await db.query(
       "UPDATE messages SET likes = likes + 1 where id = $1",
+      [messageId]
+    );
+    response.json(updatedMessage.rows[0]);
+  }
+});
+app.post("/messages/:id/dislike", async function (request, response) {
+  const messageId = request.params.id;
+  {
+    const updatedMessage = await db.query(
+      "UPDATE messages SET dislikes = dislikes + 1 where id = $1",
       [messageId]
     );
     response.json(updatedMessage.rows[0]);
