aab0a07bddd1dd01a94445e1c357591f303d418f
nuno manuel goncalves da silva pereira <schwarz14x@gmail.com>
2024-09-11 15:00:45 +0100

    fix

--- server/seed.js
+++
@@ -1,5 +0,0 @@
-async function seed() {
-  const query = `INSERT INTO messages (name, message, likes, dislikes) VALUES($1, $2, $3, $4)`;
-  const result = await db.query(query, ["Ryu", "Hadouken", 2, 1]);
-}
-seed();
